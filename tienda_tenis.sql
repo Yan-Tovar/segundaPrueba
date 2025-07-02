cd0f0b725cfe3585eab4275c12e158b322ee30ae
JoanMorales1234 <jnsebasrian.07@gmial.com>
2025-07-02 16:01:55 -0500

    segundoCommit

--- tienda_tenis.sql
+++ tienda_tenis.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tienda_tenis`
 --
-CREATE DATABASE IF NOT EXISTS `tienda_tenis` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `tienda_tenis`;
+CREATE DATABASE IF NOT EXISTS `tienda_computadores` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tienda_computadores`;
 
 -- --------------------------------------------------------
 
